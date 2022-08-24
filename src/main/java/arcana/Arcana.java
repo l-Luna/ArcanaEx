038a079f890593c8b750b6fcf0b3fe4c177ee354
l-Luna <izzeldeen03@gmail.com>
2022-08-24 12:35:42 +0100

    examplemod --> arcana

---
+++ src/main/java/arcana/Arcana.java
@@ -0,0 +1,15 @@
+package arcana;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Arcana implements ModInitializer{
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger("arcana");
+	
+	@Override
+	public void onInitialize(){
+	
+	}
+}

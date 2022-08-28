33df9c336b828983ab4380655fbe260f618d9d30
l-Luna <izzeldeen03@gmail.com>
2022-08-28 14:12:45 +0100

    wand conversion

--- src/main/java/arcana/items/WandItem.java
+++ src/main/java/arcana/items/WandItem.java
@@ -1,13 +1,25 @@
 package arcana.items;
 
 import arcana.ArcanaRegistry;
+import arcana.blocks.CrucibleBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LeveledCauldronBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class WandItem extends Item{
 	
@@ -36,6 +48,28 @@ public class WandItem extends Item{
 			stacks.add(withCapAndCore(ArcanaRegistry.IRON_WAND_CAP, ArcanaRegistry.STICK_CORE));
 			stacks.add(withCapAndCore(ArcanaRegistry.GOLD_WAND_CAP, ArcanaRegistry.BLAZE_WAND_CORE));
 		}
+	}
+	
+	public ActionResult useOnBlock(ItemUsageContext context){
+		World world = context.getWorld();
+		PlayerEntity player = context.getPlayer();
+		BlockPos pos = context.getBlockPos();
+		BlockState state = world.getBlockState(pos);
+		if(state.getBlock() == Blocks.CAULDRON){
+			world.setBlockState(pos, ArcanaRegistry.CRUCIBLE.getDefaultState());
+			world.playSound(player, pos, SoundEvents.ENTITY_EVOKER_CAST_SPELL, SoundCategory.PLAYERS, 1, 1);
+			for(int i = 0; i < 20; i++)
+				world.addParticle(ParticleTypes.END_ROD, pos.getX() + world.random.nextDouble(), pos.getY() + world.random.nextDouble(), pos.getZ() + world.random.nextDouble(), 0, 0, 0);
+			return ActionResult.SUCCESS;
+		}
+		if(state.getBlock() == Blocks.CRAFTING_TABLE){
+			world.setBlockState(pos, ArcanaRegistry.ARCANE_CRAFTING_TABLE.getDefaultState());
+			world.playSound(player, pos, SoundEvents.ENTITY_EVOKER_CAST_SPELL, SoundCategory.PLAYERS, 1, 1);
+			for(int i = 0; i < 20; i++)
+				world.addParticle(ParticleTypes.END_ROD, (pos.getX() - .1f) + world.random.nextDouble() * 1.2f, (pos.getY() - .1f) + world.random.nextDouble() * 1.2f, (pos.getZ() - .1f) + world.random.nextDouble() * 1.2f, 0, 0, 0);
+			return ActionResult.SUCCESS;
+		}
+		return ActionResult.PASS;
 	}
 	
 	public static Cap capFrom(ItemStack stack){

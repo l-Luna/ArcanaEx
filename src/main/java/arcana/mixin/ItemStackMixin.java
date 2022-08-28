4cc6df9b2624a89b9206e23f9cad194e29a69b12
l-Luna <izzeldeen03@gmail.com>
2022-08-28 20:03:06 +0100

    fix aspect display for items that already have a tooltip component (e.g. bundles)

--- src/main/java/arcana/mixin/ItemStackMixin.java
+++ src/main/java/arcana/mixin/ItemStackMixin.java
@@ -3,7 +3,6 @@ package arcana.mixin;
 import arcana.aspects.ItemAspectRegistry;
 import arcana.client.AspectsTooltipData;
 import net.minecraft.client.item.TooltipData;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,13 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
-@Mixin(Item.class)
-public class ItemMixin{
-
-	// TODO: move to ItemStack so bundle aspects display actually works
+@Mixin(ItemStack.class)
+public class ItemStackMixin{
+	
 	@Inject(at = @At("RETURN"), method = "getTooltipData", cancellable = true)
-	private void applyAspectsTooltipData(ItemStack stack, CallbackInfoReturnable<Optional<TooltipData>> cir){
-		var aspects = ItemAspectRegistry.get(stack);
+	private void applyAspectsTooltipData(CallbackInfoReturnable<Optional<TooltipData>> cir){
+		var aspects = ItemAspectRegistry.get((ItemStack)(Object)this);
 		if(aspects.size() > 0)
 			cir.setReturnValue(Optional.of(new AspectsTooltipData(aspects, cir.getReturnValue().orElse(null))));
 	}

3634952e5159681a5a1305b111e78de27ac15a55
l-Luna <izzeldeen03@gmail.com>
2022-09-02 16:23:48 +0100

    fix research table placement logic

--- src/main/java/arcana/items/ResearchTableItem.java
+++ src/main/java/arcana/items/ResearchTableItem.java
@@ -17,7 +17,23 @@ public class ResearchTableItem extends BlockItem{
 	protected boolean place(ItemPlacementContext context, BlockState state){
 		// only handle the research table
 		var f = Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD;
-		return context.getWorld().setBlockState(context.getBlockPos(), state.with(left, true), f)
-		    && context.getWorld().setBlockState(context.getBlockPos().offset(context.getPlayerFacing().rotateYClockwise()), state.with(left, false), f);
+		var pos = context.getBlockPos();
+		var offset = context.getPlayerFacing().rotateYClockwise();
+		
+		// check if the right block is replaceable
+		// not that context.canPlace() offsets by hit side, which is not what we want, so we use state.canReplace
+		ItemPlacementContext rightCtx = ItemPlacementContext.offset(context, pos.offset(offset), offset);
+		if(!context.getWorld().getBlockState(pos.offset(offset)).canReplace(rightCtx)){
+			// if not, we check if we can shift the whole table to the left
+			var opp = offset.getOpposite();
+			pos = pos.offset(opp);
+			context = ItemPlacementContext.offset(context, pos, opp);
+			if(!context.getWorld().getBlockState(pos).canReplace(context))
+				// if we have solid blocks on both sides, we can't place at all
+				return false;
+		}
+		
+		return context.getWorld().setBlockState(pos, state.with(left, true), f)
+		    && context.getWorld().setBlockState(pos.offset(offset), state.with(left, false), f);
 	}
 }

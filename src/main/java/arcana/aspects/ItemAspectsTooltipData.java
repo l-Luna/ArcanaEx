eb0c9bda883709d6e63e4edc147a67594028fe3c
l-Luna <izzeldeen03@gmail.com>
2022-09-16 17:23:37 +0100

    sort aspects in tooltips properly

--- src/main/java/arcana/aspects/ItemAspectsTooltipData.java
+++ src/main/java/arcana/aspects/ItemAspectsTooltipData.java
@@ -3,6 +3,12 @@ package arcana.aspects;
 import net.minecraft.client.item.TooltipData;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Comparator;
 import java.util.List;
 
-public record ItemAspectsTooltipData(List<AspectStack> aspects, @Nullable TooltipData inner) implements TooltipData{}
+public record ItemAspectsTooltipData(List<AspectStack> aspects, @Nullable TooltipData inner) implements TooltipData{
+	
+	public ItemAspectsTooltipData{
+		aspects.sort(Comparator.comparingInt(AspectStack::amount).reversed());
+	}
+}

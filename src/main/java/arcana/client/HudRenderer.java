e724aa220b21fe0d66ed32a876580e5dae958e6f
l-Luna <izzeldeen03@gmail.com>
2024-06-18 13:51:01 +0100

    wand HUDs

--- src/main/java/arcana/client/HudRenderer.java
+++ src/main/java/arcana/client/HudRenderer.java
@@ -1,8 +1,12 @@
 package arcana.client;
 
 import arcana.ArcanaRegistry;
+import arcana.aspects.Aspect;
+import arcana.aspects.AspectMap;
+import arcana.aspects.Aspects;
 import arcana.aura.AuraChunk;
 import arcana.aura.AuraWorld;
+import arcana.items.WandItem;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
@@ -10,9 +14,13 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec2f;
 import net.minecraft.world.World;
+
+import java.util.Map;
 
 import static arcana.Arcana.arcId;
 
@@ -20,12 +28,59 @@ public class HudRenderer{
 	
 	public static final Identifier fluxMeterFrame = arcId("textures/gui/hud/flux_meter_frame.png");
 	public static final Identifier fluxMeterFilling = arcId("textures/gui/hud/flux_chaos.png");
+	public static final Identifier wandAspects = arcId("textures/gui/hud/wand_aspects.png");
+	
+	private static final Map<Aspect, Vec2f> aspectPositions = Map.of(
+			Aspects.AIR, new Vec2f(3, 11),
+			Aspects.FIRE, new Vec2f(40, 11),
+			Aspects.WATER, new Vec2f(3, 31),
+			Aspects.EARTH, new Vec2f(40, 31),
+			Aspects.ORDER, new Vec2f(21, 3),
+			Aspects.ENTROPY, new Vec2f(21, 41)
+	);
 	
 	public static void renderHud(MatrixStack matrices, float delta){
 		PlayerEntity player = MinecraftClient.getInstance().player;
 		if(player != null){
 			World world = player.world;
-			if(player.getMainHandStack().isOf(ArcanaRegistry.FLUX_METER) || player.getOffHandStack().isOf(ArcanaRegistry.FLUX_METER)){
+			ItemStack mainHand = player.getMainHandStack(),offHand = player.getOffHandStack();
+			matrices.push();
+			ItemStack wandStack;
+			if((wandStack = mainHand).getItem() instanceof WandItem || (wandStack = offHand).getItem() instanceof WandItem){
+				AspectMap aspectStacks = WandItem.aspectsFrom(wandStack);
+				Identifier coreId = WandItem.coreFrom(wandStack).id();
+				Identifier coreTexId = new Identifier(coreId.getNamespace(), "textures/gui/hud/wand_bases/" + coreId.getPath() + ".png");
+				
+				// draw bg
+				RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+				RenderSystem.setShaderColor(1, 1, 1, 1);
+				RenderSystem.setShaderTexture(0, coreTexId);
+				RenderHelper.drawTexture(matrices, 8, 8, 0, 0, 0, 64, 64, 64, 64, 1, 1, 1, 1);
+				
+				RenderSystem.setShaderTexture(0, wandAspects);
+				for(int i = 0; i < Aspects.primals.size(); i++){
+					Aspect primal = Aspects.primals.get(i);
+					float fullness = aspectStacks.get(primal) / (float)WandItem.capacity(wandStack);
+					int pixels = (int)(14 * fullness);
+					// if you have even 1 aspect, always draw at least one pixel of filling
+					if(fullness > 0)
+						pixels = Math.max(pixels, 1);
+					
+					int aspectX = (int)aspectPositions.get(primal).x, aspectY = (int)aspectPositions.get(primal).y;
+					RenderHelper.drawTexture(matrices, 8 + aspectX, 8 + aspectY + 1 + (14 - pixels), 0, i * 16, 17 + (14 - pixels), 16, pixels, 128, 128, 1f, 1f, 1f, 1f);
+					RenderHelper.drawTexture(matrices, 8 + aspectX, 8 + aspectY + 1 + (14 - pixels), 0, i * 16, 33 + (14 - pixels), 16, pixels, 128, 128, 1f, 1f, 1f, fullness);
+					RenderHelper.drawTexture(matrices, 8 + aspectX, 8 + aspectY, 0, i * 16, 0, 16, 16, 128, 128, 1f, 1f, 1f, 1f);
+				}
+				
+				ItemStack focusStack = WandItem.focusFrom(wandStack);
+				if(!focusStack.isEmpty())
+					MinecraftClient.getInstance().getItemRenderer().renderGuiItemIcon(focusStack, 8 + 21, 8 + 22);
+				
+				// for future HUD components
+				matrices.translate(70, 0, 0);
+			}
+			
+			if(mainHand.isOf(ArcanaRegistry.FLUX_METER) || offHand.isOf(ArcanaRegistry.FLUX_METER)){
 				int frame = (int)((world.getTime() + delta) % 10);
 				float flux = AuraWorld.from(world).getChunk(player.getBlockPos()).map(AuraChunk::getFlux).orElse(0f);
 				int pixHeight = (int)Math.min(flux, 100);
@@ -51,6 +106,7 @@ public class HudRenderer{
 				if(Screen.hasShiftDown())
 					MinecraftClient.getInstance().textRenderer.drawWithShadow(matrices, String.format("%.2f", flux), 47, 8 + (97 - pixHeight), -1);
 			}
+			matrices.pop();
 		}
 	}
 }

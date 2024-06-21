9bf75e611c1bb57d78153a89d8d07da0ee4213c4
l-Luna <izzeldeen03@gmail.com>
2024-06-21 01:21:30 +0100

    reintroduce weather & wrath aspects

--- src/main/java/arcana/aspects/Aspects.java
+++ src/main/java/arcana/aspects/Aspects.java
@@ -32,6 +32,7 @@ public final class Aspects{
 	
 	public static final Aspect LIFE = create("life", WATER, EARTH, 0xd2243a);
 	public static final Aspect LIGHT = create("light", AIR, FIRE, 0xffffff);
+	public static final Aspect WEATHER = create("weather", WATER, AIR, 0xccd0Df);
 	public static final Aspect CRYSTAL = create("crystal", EARTH, ORDER, 0xbeedf6);
 	public static final Aspect ENERGY = create("energy", FIRE, ORDER, 0xb22053);
 	public static final Aspect ICE = create("ice", WATER, ORDER, 0xd3f6fb);
@@ -43,24 +44,25 @@ public final class Aspects{
 	public static final Aspect METAL = create("metal", EARTH, CRYSTAL, 0x97939a);
 	public static final Aspect SLIME = create("slime", WATER, LIFE, 0xc9f181);
 	public static final Aspect TOOL = create("tool", ENERGY, EXCHANGE, 0x4c6497);
+	public static final Aspect JOURNEY = create("journey", MOVEMENT, ORDER, 0xd6b68a);
+	public static final Aspect UNDEAD = create("undead", LIFE, ENTROPY, 0x6d8e37);
 	
 	public static final Aspect FLESH = create("flesh", PLANT, SLIME, 0xa9404d);
 	public static final Aspect FABRIC = create("fabric", TOOL, PLANT, 0xae59b7);
-	public static final Aspect JOURNEY = create("journey", MOVEMENT, ORDER, 0xd6b68a);
 	public static final Aspect WEAPON = create("weapon", TOOL, FLESH, 0xd94023);
 	public static final Aspect ARMOUR = create("armour", TOOL, LIFE, 0xa589cb);
 	public static final Aspect MINING = create("mining", TOOL, METAL, 0xfaeaa8);
+	public static final Aspect FLIGHT = create("flight", JOURNEY, AIR, 0xdee9e8);
 	
 	public static final Aspect MIND = create("mind", FLESH, ENERGY, 0x74fab8);
 	public static final Aspect SENSES = create("senses", FLESH, LIGHT, 0x73b0f2);
-	public static final Aspect UNDEAD = create("undead", LIFE, ENTROPY, 0x6d8e37);
-	public static final Aspect FLIGHT = create("flight", JOURNEY, AIR, 0xdee9e8);
 	
 	public static final Aspect MACHINE = create("machine", MIND, TOOL, 0x404e6f);
-	public static final Aspect ENVY = create("envy", MIND, WEAPON, 0x6e2b71);
+	public static final Aspect ENVY = create("envy", MIND, ARMOUR, 0x6e2b71);
 	public static final Aspect SLOTH = create("sloth", MIND, ICE, 0x3dcbcb);
 	public static final Aspect GREED = create("greed", MIND, VOID, 0xf4f62d);
-	public static final Aspect MAGIC = create("magic", SENSES, ENERGY, 0xe242b3);
+	public static final Aspect WRATH = create("wrath", MIND, WEAPON, 0x6e2b71);
+	public static final Aspect MAGIC = create("magic", SENSES, ENERGY, 0xed1507);
 	
 	public static final Aspect TAINT = create("taint", MAGIC, ENTROPY, 0x903e8e);
 	public static final Aspect AURA = create("aura", MAGIC, AIR, 0xa0e2cb);

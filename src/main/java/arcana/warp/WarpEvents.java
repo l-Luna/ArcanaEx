654584a28662dc9114c3063dac98b5abc21cf347
l-Luna <izzeldeen03@gmail.com>
2024-07-01 20:18:29 +0100

    increase warp effect spacing

--- src/main/java/arcana/warp/WarpEvents.java
+++ src/main/java/arcana/warp/WarpEvents.java
@@ -35,7 +35,7 @@ public final class WarpEvents{
 		for(ServerPlayerEntity player : world.getPlayers()){
 			Researcher researcher = Researcher.from(player);
 			long elapsed = world.getTime() - researcher.getLastWarpEventTime();
-			if((elapsed >= 20 * 60 * 9) || (researcher.wasLastWarpEventPrecursor() && elapsed >= 20 * 13))
+			if((elapsed >= 20 * 60 * 12) || (researcher.wasLastWarpEventPrecursor() && elapsed >= 20 * 18))
 				triggerEligible(player);
 		}
 	}

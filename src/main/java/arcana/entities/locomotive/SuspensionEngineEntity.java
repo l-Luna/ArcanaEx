583b8b8c287c3e62357c66cf3bde5ab27ace524d
l-Luna <izzeldeen03@gmail.com>
2024-12-26 14:26:38 +0000

    work on levitational locomotive -lu

--- src/main/java/arcana/entities/locomotive/SuspensionEngineEntity.java
+++ src/main/java/arcana/entities/locomotive/SuspensionEngineEntity.java
@@ -1,10 +1,7 @@
 package arcana.entities.locomotive;
 
-import arcana.blocks.SymbolBlock;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MovementType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
@@ -14,7 +11,6 @@ import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -26,7 +22,13 @@ import software.bernie.geckolib3.util.GeckoLibUtil;
 
 public class SuspensionEngineEntity extends Entity implements IAnimatable{
 	
-	private static final TrackedData<Direction> direction = DataTracker.registerData(SuspensionEngineEntity.class, TrackedDataHandlerRegistry.FACING);
+	private static final TrackedData<Direction>
+			direction = DataTracker.registerData(SuspensionEngineEntity.class, TrackedDataHandlerRegistry.FACING),
+			targetDirection = DataTracker.registerData(SuspensionEngineEntity.class, TrackedDataHandlerRegistry.FACING);
+	private static final TrackedData<BlockPos>
+			location = DataTracker.registerData(SuspensionEngineEntity.class, TrackedDataHandlerRegistry.BLOCK_POS),
+			targetLocation = DataTracker.registerData(SuspensionEngineEntity.class, TrackedDataHandlerRegistry.BLOCK_POS);
+	private static final TrackedData<Float> movementTimer = DataTracker.registerData(SuspensionEngineEntity.class, TrackedDataHandlerRegistry.FLOAT);
 	
 	protected static final AnimationBuilder floatAnim = new AnimationBuilder().addAnimation("engine.float");
 	private final AnimationFactory animFactory = GeckoLibUtil.createFactory(this);
@@ -40,13 +42,26 @@ public class SuspensionEngineEntity extends Entity implements IAnimatable{
 	public void tick(){
 		super.tick();
 		
-		// why does it always start down??
-		if(getDirection().getAxis().isVertical())
+		// fix invalid states
+		if(getDirection().getAxis().isVertical()){
 			setDirection(Direction.NORTH);
+			setTargetDirection(Direction.NORTH);
+		}
+		// TODO: properly initialize `here`; it's possible that a faster train might want this further out anyways?
+		if(!getBlockPos().isWithinDistance(getLocation(), 4)){
+			setLocation(getBlockPos());
+			setTargetLocation(getBlockPos());
+		}
 		
 		// look the way you travel
 		setYaw(getDirection().getRotationQuaternion().toEulerXyzDegrees().getY() + 180);
 		
+		// decide on target location
+		setTargetLocation(getLocation().offset(getTargetDirection()));
+		
+		// process symbols
+		
+		/*
 		// move with speed
 		move(MovementType.SELF, getVelocity());
 		
@@ -69,7 +84,7 @@ public class SuspensionEngineEntity extends Entity implements IAnimatable{
 		}else{
 			// slow down quickly
 			setVelocity(getVelocity().lerp(Vec3d.ZERO, 0.4));
-		}
+		}*/
 	}
 	
 	public boolean collidesWith(Entity other) {
@@ -94,17 +109,66 @@ public class SuspensionEngineEntity extends Entity implements IAnimatable{
 		dataTracker.set(direction, to);
 	}
 	
+	public BlockPos getLocation(){
+		return dataTracker.get(location);
+	}
+	
+	public void setLocation(BlockPos to){
+		dataTracker.set(location, to);
+	}
+	
+	public Direction getTargetDirection(){
+		return dataTracker.get(targetDirection);
+	}
+	
+	public void setTargetDirection(Direction to){
+		dataTracker.set(targetDirection, to);
+	}
+	
+	public BlockPos getTargetLocation(){
+		return dataTracker.get(targetLocation);
+	}
+	
+	public void setTargetLocation(BlockPos to){
+		dataTracker.set(targetLocation, to);
+	}
+	
+	public float getMovementTimer(){
+		return dataTracker.get(movementTimer);
+	}
+	
+	public void setMovementTimer(float to){
+		dataTracker.set(movementTimer, to);
+	}
+	
 	protected void writeCustomDataToNbt(NbtCompound nbt){
-		if(nbt.contains("direction"))
-			nbt.putInt("direction", getDirection().getId());
+		nbt.putInt("direction", getDirection().getId());
+		nbt.putInt("targetDirection", getTargetDirection().getId());
+		BlockPos location = getLocation();
+		nbt.putInt("locationX", location.getX());
+		nbt.putInt("locationY", location.getY());
+		nbt.putInt("locationZ", location.getZ());
+		BlockPos targetLocation = getTargetLocation();
+		nbt.putInt("targetLocationX", targetLocation.getX());
+		nbt.putInt("targetLocationY", targetLocation.getY());
+		nbt.putInt("targetLocationZ", targetLocation.getZ());
+		nbt.putFloat("movementTimer", getMovementTimer());
 	}
 	
 	protected void readCustomDataFromNbt(NbtCompound nbt){
 		setDirection(Direction.byId(nbt.getInt("direction")));
+		setTargetDirection(Direction.byId(nbt.getInt("targetDirection")));
+		setLocation(new BlockPos(nbt.getInt("locationX"), nbt.getInt("locationY"), nbt.getInt("locationZ")));
+		setTargetLocation(new BlockPos(nbt.getInt("targetLocationX"), nbt.getInt("targetLocation"), nbt.getInt("targetLocationZ")));
+		setMovementTimer(nbt.getFloat("movementTimer"));
 	}
 	
 	protected void initDataTracker(){
 		dataTracker.startTracking(direction, Direction.NORTH);
+		dataTracker.startTracking(targetDirection, Direction.NORTH);
+		dataTracker.startTracking(location, BlockPos.ORIGIN);
+		dataTracker.startTracking(targetLocation, BlockPos.ORIGIN);
+		dataTracker.startTracking(movementTimer, 0f);
 	}
 	
 	// networking

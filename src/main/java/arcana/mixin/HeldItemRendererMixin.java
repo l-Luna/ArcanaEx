421249d6b6fb7bd540274ffee6e5e4cfd7aec2cb
l-Luna <izzeldeen03@gmail.com>
2025-09-03 01:18:25 +0200

    continuous casting first person pose -lu

--- src/main/java/arcana/mixin/HeldItemRendererMixin.java
+++ src/main/java/arcana/mixin/HeldItemRendererMixin.java
@@ -6,8 +6,9 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.Quaternion;
+import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,7 +28,13 @@ public class HeldItemRendererMixin{
 		Caster caster = Caster.from(player);
 		if(caster.isDraining()){
 			matrices.translate(0, -0.1, 0);
-			matrices.multiply(Quaternion.fromEulerXyz(-0.8f, 0, 0));
+			matrices.multiply(Vec3f.POSITIVE_X.getRadialQuaternion(-0.8f));
+		}else if(caster.isContinuousCasting()){
+			// cross-reference with crossbow rendering
+			Arm arm = hand == Hand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
+			int offset = arm == Arm.RIGHT ? 1 : -1;
+			matrices.translate(offset * -0.565f, 0, 0);
+			matrices.multiply(Vec3f.POSITIVE_X.getRadialQuaternion(-0.9f));
 		}
 	}
 }

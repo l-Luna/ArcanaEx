22a5b9754fbaeca3ec45180161a00da92af3a90c
l-Luna <izzeldeen03@gmail.com>
2026-01-02 21:17:11 +0000

    BotT reducing fall damage -lu

--- src/main/java/arcana/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/arcana/mixin/ClientPlayerEntityMixin.java
@@ -1,22 +1,30 @@
 package arcana.mixin;
 
 import arcana.items.BootsOfTheTravellerItem;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.encryption.PlayerPublicKey;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ClientPlayerEntity.class)
-public class ClientPlayerEntityMixin{
+public abstract class ClientPlayerEntityMixin extends PlayerEntity{
+	
+	public ClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile, PlayerPublicKey publicKey){
+		super(world, pos, yaw, gameProfile, publicKey);
+	}
 	
 	// change auto-jump threshold with step assist
 	@ModifyArg(method = "autoJump",
 	           at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;add(DDD)Lnet/minecraft/util/math/Vec3d;"),
 	           index = 1)
 	private double changeAutoJumpRequiredHeight(double y){
-		if(y == 0.51f && ((LivingEntity)(Object)this).getEquippedStack(EquipmentSlot.FEET).getItem() instanceof BootsOfTheTravellerItem)
+		if(y == 0.51f && getEquippedStack(EquipmentSlot.FEET).getItem() instanceof BootsOfTheTravellerItem)
 			return 1.01;
 		return y;
 	}
--- src/main/java/arcana/mixin/PlayerEntityMixin.java
+++ src/main/java/arcana/mixin/PlayerEntityMixin.java
@@ -1,7 +1,13 @@
 package arcana.mixin;
 
 import arcana.blocks.WardedCampfireBlock;
-import net.minecraft.entity.Entity;
+import arcana.items.BootsOfTheTravellerItem;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
@@ -9,20 +15,44 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin{
+public abstract class PlayerEntityMixin extends LivingEntity{
+	
+	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world){
+		super(entityType, world);
+	}
 	
 	@Shadow public abstract HungerManager getHungerManager();
 	
+	@Shadow public abstract ItemStack getEquippedStack(EquipmentSlot slot);
+	
 	@Inject(method = "eatFood", at = @At("HEAD"))
 	private void applyWardedCampfireFoodBonus(World world, ItemStack stack, CallbackInfoReturnable<ItemStack> cir){
-		if(stack.isFood() && WardedCampfireBlock.isProtected((Entity)(Object)this)){
+		if(stack.isFood() && WardedCampfireBlock.isProtected(this)){
 			FoodComponent food = stack.getItem().getFoodComponent();
 			getHungerManager().add(food.getHunger() / 2, food.getSaturationModifier() / 2);
 		}
+	}
+	
+	// also see: ClientPlayerEntityMixin
+	@WrapMethod(method = "damage")
+	private boolean applyDamage(DamageSource source, float amount, Operation<Boolean> original){
+		if(source.isFromFalling()){
+			if(amount > 0 && amount <= effectiveFallDamageReduction())
+				return false;
+			return original.call(source, amount - effectiveFallDamageReduction());
+		}
+		return original.call(source, amount);
+	}
+	
+	@Unique
+	private int effectiveFallDamageReduction(){
+		boolean boostFromBoots = getEquippedStack(EquipmentSlot.FEET).getItem() instanceof BootsOfTheTravellerItem;
+		return boostFromBoots ? 3 : 0;
 	}
 }

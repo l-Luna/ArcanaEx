9062f04b7ea294c4d8f077f7736afb40dfbda42e
l-Luna <izzeldeen03@gmail.com>
2026-01-05 23:41:42 +0000

    some royal bullshit -lu

--- src/main/java/arcana/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/arcana/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.registry.Registry;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,8 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class AbstractFurnaceBlockEntityMixin{
 	
 	@Inject(method = "craftRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;increment(I)V"))
-	private static void applyCookingRecipeAmount(@Nullable Recipe<?> recipe, DefaultedList<ItemStack> slots, int count, CallbackInfoReturnable<Boolean> cir){
-		assert recipe != null; // already checked before injection
+	private static void applyCookingRecipeAmount(Recipe<?> recipe, DefaultedList<ItemStack> slots, int count, CallbackInfoReturnable<Boolean> cir){
 		ItemStack inputSlot = slots.get(0);
 		// don't mess with non-arcana recipes
 		if(!Registry.ITEM.getId(inputSlot.getItem()).getNamespace().equals(Arcana.modid))

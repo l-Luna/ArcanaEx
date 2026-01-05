0ba5420dd131359d4daedef4761e4ded47d2e7a4
l-Luna <izzeldeen03@gmail.com>
2026-01-05 23:28:05 +0000

    adjust furnace mixin to avoid potential mod conflict -lu

--- src/main/java/arcana/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/arcana/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,9 +1,11 @@
 package arcana.mixin;
 
+import arcana.Arcana;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,13 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin{
 	
-	@Inject(method = "craftRecipe",
-	        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;increment(I)V", shift = At.Shift.AFTER))
+	@Inject(method = "craftRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;increment(I)V"))
 	private static void applyCookingRecipeAmount(@Nullable Recipe<?> recipe, DefaultedList<ItemStack> slots, int count, CallbackInfoReturnable<Boolean> cir){
 		assert recipe != null; // already checked before injection
+		ItemStack inputSlot = slots.get(0);
+		// don't mess with non-arcana recipes
+		if(!Registry.ITEM.getId(inputSlot.getItem()).getNamespace().equals(Arcana.modid))
+			return;
 		ItemStack outputSlot = slots.get(2);
-		// MC's already added 1
-		// TODO: avoid incompatibility with other mods doing the same
+		// MC's already added 1, just add remainder
 		outputSlot.increment(recipe.getOutput().getCount() - 1);
 	}
 }

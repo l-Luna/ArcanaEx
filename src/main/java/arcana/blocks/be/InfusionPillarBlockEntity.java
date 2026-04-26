abe0bd00ce9ee6af003810a1f30469dae02fe3b1
l-Luna <izzeldeen03@gmail.com>
2026-04-26 18:41:32 +0100

    fix infusion altars breaking on save&quit -lu

--- src/main/java/arcana/blocks/be/InfusionPillarBlockEntity.java
+++ src/main/java/arcana/blocks/be/InfusionPillarBlockEntity.java
@@ -3,6 +3,10 @@ package arcana.blocks.be;
 import arcana.ArcanaRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.Packet;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,5 +24,29 @@ public class InfusionPillarBlockEntity extends BlockEntity{
 	
 	public void setMatrixPosition(@Nullable BlockPos matrixPos){
 		relativeMatrixPosition = matrixPos != null ? matrixPos.subtract(pos) : null;
+		markDirty();
+	}
+	
+	protected void writeNbt(NbtCompound nbt){
+		super.writeNbt(nbt);
+		nbt.putBoolean("active", relativeMatrixPosition != null);
+		if(relativeMatrixPosition != null)
+			nbt.putLong("relativeMatrixPosition", relativeMatrixPosition.asLong());
+	}
+	
+	public void readNbt(NbtCompound nbt){
+		super.readNbt(nbt);
+		if(nbt.getBoolean("active"))
+			relativeMatrixPosition = BlockPos.fromLong(nbt.getLong("relativeMatrixPosition"));
+		else
+			relativeMatrixPosition = null;
+	}
+	
+	public Packet<ClientPlayPacketListener> toUpdatePacket() {
+		return BlockEntityUpdateS2CPacket.create(this);
+	}
+	
+	public NbtCompound toInitialChunkDataNbt() {
+		return createNbt();
 	}
 }

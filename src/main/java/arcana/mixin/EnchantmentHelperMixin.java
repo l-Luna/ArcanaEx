cbb9d2a6d81bc9ab66d67e3a4c2597e5cd372478
l-Luna <izzeldeen03@gmail.com>
2026-05-01 16:17:33 +0100

    use `ModifyReturnValue` in `EnchantmentHelperMixin` -lu

--- src/main/java/arcana/mixin/EnchantmentHelperMixin.java
+++ src/main/java/arcana/mixin/EnchantmentHelperMixin.java
@@ -1,23 +1,19 @@
 package arcana.mixin;
 
 import arcana.ArcanaRegistry;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import static net.minecraft.enchantment.EnchantmentHelper.getEquipmentLevel;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin{
 
-	@Inject(method = "getFireAspect", at = @At("HEAD"), cancellable = true)
-	private static void getFireAspect(LivingEntity entity, CallbackInfoReturnable<Integer> cir){
-		int orig = getEquipmentLevel(Enchantments.FIRE_ASPECT, entity);
-		if(entity.hasStatusEffect(ArcanaRegistry.FIRE_POWER))
-			cir.setReturnValue(Math.max(orig, 1));
+	//
+	
+	@ModifyReturnValue(method = "getFireAspect", at = @At("RETURN"))
+	private static int getFireAspect(int original, LivingEntity entity){
+		return entity.hasStatusEffect(ArcanaRegistry.FIRE_POWER) ? (Math.max(original, 1)) : original;
 	}
 }

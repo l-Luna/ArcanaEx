ada6bdb7fac91cb2bf8f17534867c2f7c3b85a74
l-Luna <izzeldeen03@gmail.com>
2026-06-18 20:02:18 +0100

    reimpl cube particle effect (shader pending)

--- src/main/java/arcana/client/particles/CubeParticle.java
+++ src/main/java/arcana/client/particles/CubeParticle.java
@@ -1,13 +1,18 @@
 package arcana.client.particles;
 
+import arcana.client.RenderHelper;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleFactory;
 import net.minecraft.client.particle.ParticleTextureSheet;
 import net.minecraft.client.particle.SpriteProvider;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 
 public class CubeParticle extends Particle{
@@ -28,25 +33,24 @@ public class CubeParticle extends Particle{
 	
 	public void buildGeometry(VertexConsumer vc, Camera camera, float tickDelta){
 		// TODO: veiling
-		/*RenderSystem.setShader(ArcanaShaders::getFxTurbulentShader);
-		RenderSystem.getShader().getUniformOrDefault("TurbulenceColor").set(effectR, effectG, effectB);
+		RenderSystem.setShader(GameRenderer::getPositionColorTexLightmapProgram);
+		//RenderSystem.getShader().getUniformOrDefault("TurbulenceColor").set(effectR, effectG, effectB);
 		
-		BufferBuilder buffer = Tessellator.getInstance().getBuffer();
 		RenderSystem.enableBlend();
 		RenderSystem.depthMask(true);
 		RenderSystem.setShaderTexture(0, SpriteAtlasTexture.PARTICLE_ATLAS_TEXTURE);
-		buffer.begin(VertexFormat.DrawMode.QUADS, ArcanaShaders.FX);
+		BufferBuilder buffer = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL);
 		
 		float age = (this.age + tickDelta) / (maxAge + 1);
 		float s = 1.01f * style.easeScale(age), o = (s - 1) / 2;
 		int alpha1 = (int)(style.easeAlpha(age) * 200);
 		int c = ColorHelper.Argb.getArgb(alpha1, (int)(red * 255), (int)(green * 255), (int)(blue * 255));
 		Vec3d d = new Vec3d(x, y, z).add(style.easeOffset(age, random));
-		RenderHelper.colCuboid(buffer, new MatrixStack(), c, d.subtract(camera.getPos()).subtract(o, o, o), s, sprite, true);
+		RenderHelper.colCuboid(buffer, new MatrixStack(), c, d.subtract(camera.getPos()).subtract(o, o, o), s, sprite, false);
 		
-		Tessellator.getInstance().draw();
+		RenderHelper.drawBuffer(buffer);
 		
-		RenderSystem.setShader(GameRenderer::getParticleShader);*/
+		RenderSystem.setShader(GameRenderer::getParticleProgram);
 	}
 	
 	public ParticleTextureSheet getType(){

d180db3a7ccfda859d209fd73a7e201920336af7
l-Luna <izzeldeen03@gmail.com>
2026-06-21 18:06:43 +0100

    add discount amount to adorned ring tooltip

--- src/main/java/arcana/items/VisDiscountTrinketItem.java
+++ src/main/java/arcana/items/VisDiscountTrinketItem.java
@@ -3,9 +3,15 @@ package arcana.items;
 import arcana.api.VisDiscountingItem;
 import arcana.aspects.Aspect;
 import dev.emi.trinkets.api.TrinketItem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class VisDiscountTrinketItem extends TrinketItem implements VisDiscountingItem{
 	
@@ -18,5 +24,11 @@ public class VisDiscountTrinketItem extends TrinketItem implements VisDiscountin
 	
 	public int percentOff(ItemStack stack, @Nullable Aspect aspect, PlayerEntity player){
 		return percentOff;
+	}
+	
+	@Environment(EnvType.CLIENT)
+	public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type){
+		super.appendTooltip(stack, context, tooltip, type);
+		tooltip.add(Text.translatable("tooltip.arcana.wand.discount.all", percentOff));
 	}
 }
